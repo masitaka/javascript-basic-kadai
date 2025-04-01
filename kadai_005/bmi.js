dc8ab4c2ed55ecfdc71b6b6d246e10f36129d45d
Matsushita Masaru <masitaka69@gmail.com>
2025-04-01 12:17:50 +0900

    課題10

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 const weight = 68;
 const height = 1.7;
 const bmi = weight / (height * height);
 
-=======
-const weight = 68;
-const height = 1.7;
-const bmi = weight / (height * height);
-
->>>>>>> e54e6d25a11910451e73096bed84e085c1c1b210
 console.log(bmi);
